bb21b126a3e7302299a4961f20c64fbf8199e46a
Shakeel Khan <shinwari.khan474@gmail.com>
2023-04-08 18:19:56 +0500

    submitting ansers

--- Section-4/section-4 solution/utils/balance.js
+++
@@ -1,5 +0,0 @@
-const ethers = require('ethers');
-
-module.exports = async(provider, address) => {
-    return ethers.utils.formatEther(await provider.getBalance(address));
-}
--- Section-4/section-4 solution/utils/signer.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const ethers = require('ethers');
-const provider = require('./provider')
-
-module.exports = new ethers.Wallet(process.env.PRIVATE_KEY).connect(provider);
